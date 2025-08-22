083fe807019a220be2ed31399247f005f37a11a7
Doug Hatcher <doug.hatcher@blueacornici.com>
2025-08-22 15:50:50 +0000

    allow-popups

--- src/commerce-backend-ui-1/actions/registration/index.js
+++ src/commerce-backend-ui-1/actions/registration/index.js
@@ -33,6 +33,7 @@ function main() {
             title: "Store Locator",
             isSection: false,
             sortOrder: 100,
+            sandbox: "allow-popups"
           },
         ],
         page: {
